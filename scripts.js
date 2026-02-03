e932c932d4cf44fcfeabc86919499d03b7a9b6ec
Pablo SanTat <pablokrak88@gmail.com>
2026-02-03 10:16:25 +0100

    update
    Añadi 2 cajas de imagenes de slider y los botones redireccionan a la pagina web

--- scripts.js
+++ scripts.js
@@ -2,14 +2,23 @@ let index = 0;
 const slides = document.querySelectorAll('.slide');
 
 function cambiarImagen() {
-    slides[index].classList.remove('active');
-    index = (index + 1) % slides.length;
-    slides[index].classList.add('active');
+
+    // Quitar "active" de TODAS las imágenes
+    slides.forEach(slide => slide.classList.remove('active'));
+
+    // Aumentar índice
+    index = (index + 1) % (slides.length / 2); 
+    // Dividimos entre 2 porque ahora hay 2 sliders con las mismas imágenes
+
+    // Activar la imagen correspondiente en AMBOS sliders
+    document.querySelectorAll('.lado-izquierdo .slide')[index].classList.add('active');
+    document.querySelectorAll('.lado-derecho .slide')[index].classList.add('active');
 }
 
-setInterval(cambiarImagen, 3000);
+// ⏱️ Cambian cada 7 segundos Y A LA VEZ
+setInterval(cambiarImagen, 7000);
 
-// Número WhatsApp aleatorio
+// Número WhatsApp
 document.getElementById("whatsapp").innerText =
     "+34 692 19 94 07";
 
